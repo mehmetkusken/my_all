0f54e668e38453db6dc5c7f0c808920407fcbf00
Mehmet <mehmetkusken@gmail.com>
2021-02-09 20:02:00 -0700

    commit tree 609a3dc4f01055edbb87567a9cdca546fbf1722c

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
 def my_all?(collection)
+    
+    i = 0
+    
+    return_values = [1, 2, 3]
+   
+    while i<collection.length
+        return_values << yield(collection[i])
+        i = i + 1
+    end
+        if return_values.include?(false)
+            false
+        else
+            true
+        end
+    
+    
 
 end
